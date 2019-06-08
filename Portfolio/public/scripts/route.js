f679b4dae1781d353b64ea608c2c91072780cf05
Saurabh singh <2908aman@gmail.com>
2019-06-09 02:09:49 +0530

    Single Page Application in AngularJs

---
+++ Portfolio/public/scripts/route.js
@@ -0,0 +1,19 @@
+app.config(function($routeProvider, $locationProvider){
+    $locationProvider.hashPrefix('');
+    
+    $routeProvider.when('/portfolio',{
+        templateUrl:'views/portfolio.html'
+    }).when('/Resume',{
+        templateUrl:'views/resume.html',
+    })
+    .when('/About',{
+        templateUrl:'views/about.html',
+       
+    })
+    .when('/Projects',{
+        templateUrl:'views/project.html'
+    })
+    .when('/Contact',{
+        templateUrl:'views/contact.html'
+    })
+})
